280bb5cc261f9874c73d431e92cf63e1b4417a57
ngocviet <lengocviet31983@gmail.com>
2024-04-01 16:18:20 +0700

    v

---
+++ ModelView/ModelViewLearnWord.cs
@@ -0,0 +1,18 @@
+﻿namespace project4.ModelView
+{
+    public class ModelViewLearnWord
+    {
+    }
+    public class ItemGetDataQuestion
+    {
+        public string Code { get; set; }
+        public int Stt { get; set; }
+    }
+    public class ItemAnswer
+    {
+        public string Code { get; set; }
+        public bool IsCorrect { get; set; }
+        public string NameEN { get; set; }
+        public string NameVN { get; set; }
+    }
+}
