fac44a7edce7b246634aa0fdc741c24e548f9f28
DoubleH106 <quanghuong106@gmail.com>
2024-04-01 09:16:30 +0700

    huong commit

---
+++ Controllers/LearnWordController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project4.model;
+
+namespace project4.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LearnWordController : ControllerBase
+	{
+		private readonly project4Context _context;
+
+		public LearnWordController(project4Context context)
+		{
+			_context = context;
+		}
+		[HttpPost("getDataQuestion")]
+		public async Task<IActionResult> getDataQuestion()
+		{
+			var data = _context.Word
+				.Where(x => x.IsDeleted == false)
+				.Select(x => new
+				{
+					x.Avatar,
+					x.NameEN,
+					x.NameVN,
+				}).FirstOrDefault();
+			return Ok(data);
+		}
+
+
+		[HttpPost("getDataAnwer")]
+		public async Task<IActionResult> getDataAnwer()
+		{
+			//var data = _context.Word.Where(x=>x.IsDeleted == false)
+			return Ok();
+		}
+	}
+}
