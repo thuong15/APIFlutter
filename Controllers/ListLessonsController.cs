3c21f6ced8e23eb2ff44d1996b82aba826a035c2
DoubleH106 <quanghuong106@gmail.com>
2024-04-09 16:07:07 +0700

    huong commit API List_Lesson

--- Controllers/ListLessonsController.cs
+++ Controllers/ListLessonsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using project4.model;
+using project4.ModelView;
 
 namespace project4.Controllers
 {
@@ -15,20 +16,25 @@ namespace project4.Controllers
 			_context = context;
 		}
 		[HttpPost("getListLisson")]
-		public async Task<IActionResult> getListLesson()
+		public async Task<IActionResult> getListLesson([FromBody] ModelViewListLesson modelViewListLesson)
 		{
 			var data = from a in _context.Lesson.Where(x => x.IsDeleted == false)
-						join b in _context.History.Where(x => x.IsDeleted == false && x.UserCode == "viet_1")
-						on a.Code equals b.LessonCode
-						group a by a.Code into lessonGroup
-						select new
-						{
-							LessonCode = lessonGroup.Key,
-							Total = lessonGroup.Count(),
-							Name = lessonGroup.Select(a => a.Name).FirstOrDefault(),
-						};
-
-			return Ok(data);
+					   join b in _context.History.Where(x => x.IsDeleted == false && x.UserCode == modelViewListLesson.codeUser && x.IsNew == true)
+					   on a.Code equals b.LessonCode
+					   group a by a.Code into lessonGroup
+					   select new
+					   {
+						   LessonCode = lessonGroup.Key,
+						   Name = lessonGroup.Select(a => a.Name).FirstOrDefault(),
+						   totalquestion = _context.Question.Where(x => x.LessonCode == lessonGroup.Key && x.IsDeleted == false).Count(),
+                        };
+            var totalCount = data.Count();
+			var container = new
+			{
+				totalCount = totalCount,
+                data = data,
+			};
+            return Ok(container);
 		}
 	}
 }
---
+++ ModelView/ModelViewListLesson.cs
@@ -0,0 +1,7 @@
+﻿namespace project4.ModelView
+{
+    public class ModelViewListLesson
+    {
+        public String codeUser { get;set; }
+    }
+}
