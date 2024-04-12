1edff2811179c905f608853ec9b2fb7aea6dd026
DoubleH106 <quanghuong106@gmail.com>
2024-04-12 08:38:50 +0700

    huong commit update Ratings

--- Controllers/RatingsController.cs
+++ Controllers/RatingsController.cs
@@ -23,25 +23,29 @@ namespace project4.Controllers
             DateTime Date = DateTime.Now;
             DateTime startDate = new DateTime(Date.Year, Date.Month, 1);
             DateTime endDate = startDate.AddMonths(1).AddDays(-1);
+            var startDatePreviousMonth = startDate.AddMonths(-1);
+            var endDatePreviousMonth = endDate.AddMonths(-1);
 
-            var user = (from a in _context.Account
-                        join b in _context.History
-                        on a.Code equals b.UserCode
-                        where a.IsDeleted == false
-                              && a.Code == modelViewRetings.UserCode
-                              && b.IsDeleted == false
-                              && b.IsNew == true
-                              && b.IsCorrect == true
-                              && b.CreatedTime >= startDate && b.CreatedTime <= endDate
-                        group a by b.UserCode into groupedData
-                        select new
-                        {
-                            totalscore = groupedData.Count(),
-                            name = groupedData.Select(x => x.Name).FirstOrDefault(),
-                            avatar = groupedData.Select(x => x.Avatar).FirstOrDefault(),
-                        }).FirstOrDefault();
+            var dataHistory = (from a in _context.Account.Where(x => x.IsDeleted == false)
+                               join b in _context.History.Where(x => x.IsDeleted == false && x.IsNew == true && x.IsCorrect == true && x.CreatedTime >= startDatePreviousMonth && x.CreatedTime <= endDatePreviousMonth)
+                               on a.Code equals b.UserCode
+                               select new
+                               {
+                                   a.Code,
+                                   a.Avatar,
+                                   a.Name,
+                                   b.IsCorrect,
+                               }into c
+                               group c by new { c.Code } into groupedData
+                               select new
+                               {
+                                   groupedData.Key.Code,
+                                   groupedData.First().Name,
+                                   groupedData.First().Avatar,
+                                   totalscore = groupedData.Count(x => x.IsCorrect),
+                               }).OrderByDescending(x => x.totalscore).FirstOrDefault();
 
-			var data = (from a in _context.Account.Where(x => x.IsDeleted == false)
+            var data = (from a in _context.Account.Where(x => x.IsDeleted == false)
                         join b in _context.History.Where(x => x.IsDeleted == false && x.IsNew == true && x.IsCorrect == true && x.CreatedTime >= startDate && x.CreatedTime <= endDate)
                         on a.Code equals b.UserCode
                         select new
@@ -60,35 +64,12 @@ namespace project4.Controllers
                             isUser = groupedData.First().Code == modelViewRetings.UserCode ? true : false,
 							totalscore = groupedData.Count(x => x.IsCorrect),
                         }).OrderByDescending(x => x.totalscore);
-            var userPosition = data.ToList().FindIndex(x => x.Key == modelViewRetings.UserCode) + 1;
             var result = new
             {
-                TotalScoreByUser = user,
+                dataHistory = dataHistory,
                 TotalScoreByAllUsers = data,
-                Reting = userPosition,
             };
             return Ok(result);
         }
-        [HttpPost("RankLastMonth")]
-        public async Task<IActionResult> RankLastMonth()
-        {
-            DateTime Date = DateTime.Now;
-            DateTime startDate = new DateTime(Date.Year, Date.Month, 1);
-            DateTime endDate = startDate.AddMonths(1).AddDays(-1);
-            var startDatePreviousMonth = startDate.AddMonths(-1);
-            var endDatePreviousMonth = endDate.AddMonths(-1);
-
-            var dataHistory = (from a in _context.Account.Where(x => x.IsDeleted == false)
-                               join b in _context.History.Where(x => x.IsDeleted == false && x.IsNew == true && x.IsCorrect == true && x.CreatedTime >= startDatePreviousMonth && x.CreatedTime <= endDatePreviousMonth)
-                               on a.Code equals b.UserCode
-                               group b by new { b.UserCode } into groupedData
-                               select new
-                               {
-                                   groupedData.Key.UserCode,
-                                   totalscore = groupedData.Count(x => x.IsCorrect),
-                               }).OrderByDescending(x => x.totalscore).FirstOrDefault();
-            return Ok(dataHistory);
-        }
-
     }
 }
